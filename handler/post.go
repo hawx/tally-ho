e8f82e4f52a641d75ac7179e6ec44a0e2120831f
Joshua Hawxwell <m@hawx.me>
2019-03-05 19:00:21 +0000

    Add post handler

--- handler/post.go
+++ handler/post.go
@@ -1,28 +1,85 @@
 package handler
 
 import (
-	"io"
-	"log"
+	"encoding/json"
 	"net/http"
-	"os"
+	"strings"
+
+	"hawx.me/code/mux"
 )
 
-type postStore interface {
-	CreateEntry(content string, categories []string) error
+type JSON struct {
+	Type       []string                 `json:"type"`
+	Properties map[string][]interface{} `json:"properties"`
 }
 
-func Post(store postStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		h := r.FormValue("h")
+type postStore interface {
+	Create(data JSON) (id string, err error)
+}
 
-		switch h {
-		case "entry":
-			content := r.FormValue("content")
+func Post(store postStore) http.Handler {
+	handleJSON := func(w http.ResponseWriter, r *http.Request) {
+		data := JSON{Properties: map[string][]interface{}{}}
 
-			log.Println("new post\n\n", content)
-		default:
-			log.Println("don't know how to ", h)
-			io.Copy(os.Stdout, r.Body)
+		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+			http.Error(w, "could not decode json request: "+err.Error(), http.StatusBadRequest)
+			return
 		}
+
+		id, err := store.Create(data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Add("Location", "/"+id)
+		w.WriteHeader(http.StatusCreated)
+	}
+
+	handleForm := func(w http.ResponseWriter, r *http.Request) {
+		data := JSON{Properties: map[string][]interface{}{}}
+
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, "could not parse form: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		for key, values := range r.Form {
+			// ignore reserved property names
+			if key == "access_token" || key == "action" || key == "url" || strings.HasPrefix(key, "mp-") {
+				continue
+			}
+
+			if key == "h" {
+				data.Type = []string{"h-" + values[0]}
+			} else {
+				if strings.HasSuffix(key, "[]") {
+					key := key[:len(key)-2]
+					for _, value := range values {
+						data.Properties[key] = append(data.Properties[key], value)
+					}
+				} else {
+					data.Properties[key] = []interface{}{values[0]}
+				}
+			}
+		}
+
+		id, err := store.Create(data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Add("Location", "/"+id)
+		w.WriteHeader(http.StatusCreated)
+	}
+
+	handleMultiPart := func(w http.ResponseWriter, r *http.Request) {
+
+	}
+
+	return mux.ContentType{
+		"application/json":                  http.HandlerFunc(handleJSON),
+		"application/x-www-form-urlencoded": http.HandlerFunc(handleForm),
+		"multipart/form-data":               http.HandlerFunc(handleMultiPart),
 	}
 }
--- handler/post_test.go
+++ handler/post_test.go
@@ -4,23 +4,19 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strings"
 	"testing"
 
 	"hawx.me/code/assert"
 )
 
-type fakeEntry struct {
-	content    string
-	categories []string
-}
-
 type fakePostStore struct {
-	entries []fakeEntry
+	datas []JSON
 }
 
-func (s *fakePostStore) CreateEntry(content string, categories []string) error {
-	s.entries = append(s.entries, fakeEntry{content, categories})
-	return nil
+func (s *fakePostStore) Create(data JSON) (string, error) {
+	s.datas = append(s.datas, data)
+	return "1", nil
 }
 
 func TestPostEntry(t *testing.T) {
@@ -30,15 +26,51 @@ func TestPostEntry(t *testing.T) {
 	s := httptest.NewServer(Post(store))
 	defer s.Close()
 
-	http.PostForm(s.URL, url.Values{
+	resp, err := http.PostForm(s.URL, url.Values{
 		"h":          {"entry"},
 		"content":    {"This is a test"},
 		"category[]": {"test", "ignore"},
 	})
 
-	assert.Len(store.entries, 1)
+	assert.Nil(err)
+	assert.Equal(http.StatusCreated, resp.StatusCode)
+	assert.Equal("/1", resp.Header.Get("Location"))
+
+	if assert.Len(store.datas, 1) {
+		data := store.datas[0]
+
+		assert.Equal("h-entry", data.Type[0])
+		assert.Equal("This is a test", data.Properties["content"][0])
+		assert.Equal("test", data.Properties["category"][0])
+		assert.Equal("ignore", data.Properties["category"][1])
+	}
 }
 
 func TestPostEntryJSON(t *testing.T) {
-	t.Fatal("todo")
+	assert := assert.New(t)
+	store := &fakePostStore{}
+
+	s := httptest.NewServer(Post(store))
+	defer s.Close()
+
+	resp, err := http.Post(s.URL, "application/json", strings.NewReader(`{
+  "type": ["h-entry"],
+  "properties": {
+    "content": ["This is a test"],
+    "category": ["test", "ignore"]
+  }
+}`))
+
+	assert.Nil(err)
+	assert.Equal(http.StatusCreated, resp.StatusCode)
+	assert.Equal("/1", resp.Header.Get("Location"))
+
+	if assert.Len(store.datas, 1) {
+		data := store.datas[0]
+
+		assert.Equal("h-entry", data.Type[0])
+		assert.Equal("This is a test", data.Properties["content"][0])
+		assert.Equal("test", data.Properties["category"][0])
+		assert.Equal("ignore", data.Properties["category"][1])
+	}
 }
