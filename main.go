3d7bc796cef655ce8e16b5c414c1b6daf53a90d0
Joshua Hawxwell <m@hawx.me>
2019-03-13 19:21:51 +0000

    Start adding numbersix

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"flag"
 	"log"
 
+	"github.com/google/uuid"
 	"hawx.me/code/mux"
+	"hawx.me/code/numbersix"
 	"hawx.me/code/route"
 	"hawx.me/code/serve"
+	"hawx.me/code/tally-ho/handler"
 )
 
 func main() {
@@ -21,8 +24,13 @@ func main() {
 		log.Fatal("--me must be provided")
 	}
 
+	store, err := newStore("file::memory:")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	route.Handle("/micropub", mux.Method{
-	// "POST": handler.Authenticate(*me, "create", handler.Post()),
+		"POST": handler.Authenticate(*me, "create", handler.Post(store)),
 	})
 
 	route.Handle("/webmention", mux.Method{
@@ -30,4 +38,33 @@ func main() {
 	})
 
 	serve.Serve(*port, *socket, route.Default)
+}
+
+func newStore(path string) (*Store, error) {
+	db, err := numbersix.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Store{db: db}, nil
+}
+
+type Store struct {
+	db *numbersix.DB
+}
+
+func (s *Store) Create(data map[string][]interface{}) (string, error) {
+	id := uuid.New().String()
+
+	for key, value := range data {
+		if err := s.db.InsertMany(id, key, value); err != nil {
+			return "", err
+		}
+	}
+
+	return id, nil
+}
+
+func (s *Store) Update(url string, replace, add, delete map[string][]interface{}) error {
+	return nil
 }
