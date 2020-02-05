be17a28d3a81003af1d164e5cf50c070b9fb82e0
Joshua Hawxwell <m@hawx.me>
2020-02-05 10:34:41 +0000

    Start adding a websub hub implementation

---
+++ websub/hub.go
@@ -0,0 +1,138 @@
+package websub
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+const defaultLease = 28 * 24 * time.Hour
+
+type HubStore interface {
+	Subscribe(callback, topic string, expiresAt time.Time) error
+	Subscribers(topic string) ([]string, error)
+	Unsubscribe(callback, topic string) error
+}
+
+func New(store HubStore) *Hub {
+	hub := &Hub{Store: store}
+
+	return hub
+}
+
+type Hub struct {
+	Store HubStore
+}
+
+func (h *Hub) Handler() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "only POST is allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var (
+			callback = r.FormValue("hub.callback")
+			mode     = r.FormValue("hub.mode")
+			topic    = r.FormValue("hub.topic")
+		)
+
+		callbackURL, err := url.Parse(callback)
+		if err != nil || !callbackURL.IsAbs() {
+			http.Error(w, "hub.callback must be a url", http.StatusBadRequest)
+			return
+		}
+
+		if mode != "subscribe" && mode != "unsubscribe" {
+			http.Error(w, "hub.mode must be subscribe or unsubscribe", http.StatusBadRequest)
+			return
+		}
+
+		query := callbackURL.Query()
+		query.Add("hub.mode", mode)
+		query.Add("hub.topic", topic)
+		query.Add("hub.challenge", "this-is-a-challenge")
+		query.Add("hub.lease_seconds", strconv.Itoa(int(defaultLease.Seconds())))
+		callbackURL.RawQuery = query.Encode()
+
+		resp, err := http.Get(callbackURL.String())
+		if err != nil {
+			http.Error(w, "problem requesting hub.callback", http.StatusBadRequest)
+			return
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+			http.Error(w, "hub.callback returned a non-200 response", http.StatusBadRequest)
+			return
+		}
+
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, "", http.StatusInternalServerError)
+		}
+
+		if string(data) != "this-is-a-challenge" {
+			http.Error(w, "hub.challenge must match", http.StatusBadRequest)
+			return
+		}
+
+		h.Store.Subscribe(callback, topic, time.Now().Add(defaultLease))
+		w.WriteHeader(http.StatusAccepted)
+	})
+
+	return mux
+}
+
+func (h *Hub) Publish(topic string) error {
+	resp, err := http.Get(topic)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		// todo
+	}
+
+	contentType := resp.Header.Get("Content-Type")
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	subscribers, err := h.Store.Subscribers(topic)
+	if err != nil {
+		// todo
+	}
+
+	// todo: no redirect client
+	client := http.DefaultClient
+	link := `<the-hub-url>; rel="hub", <` + topic + `>; rel="self"`
+
+	for _, subscriber := range subscribers {
+		req, err := http.NewRequest("POST", subscriber, bytes.NewReader(body))
+		if err != nil {
+			continue
+		}
+
+		req.Header.Add("Content-Type", contentType)
+		req.Header.Add("Link", link)
+		resp, err := client.Do(req)
+		if err != nil {
+			continue
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode == http.StatusGone {
+			h.Store.Unsubscribe(subscriber, topic)
+		}
+	}
+
+	return nil
+}
---
+++ websub/hub_test.go
@@ -0,0 +1,244 @@
+package websub
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+	"time"
+
+	"hawx.me/code/assert"
+)
+
+type fakeSub struct {
+	callback  string
+	topic     string
+	expiresAt time.Time
+}
+
+type fakeHubStore struct {
+	subs   []fakeSub
+	unsubs []fakeSub
+}
+
+func (s *fakeHubStore) Subscribe(callback, topic string, expiresAt time.Time) error {
+	s.subs = append(s.subs, fakeSub{callback, topic, expiresAt})
+	return nil
+}
+
+func (s *fakeHubStore) Subscribers(topic string) ([]string, error) {
+	var subs []string
+	for _, sub := range s.subs {
+		if sub.topic == topic {
+			subs = append(subs, sub.callback)
+		}
+	}
+	return subs, nil
+}
+
+func (s *fakeHubStore) Unsubscribe(callback, topic string) error {
+	s.unsubs = append(s.unsubs, fakeSub{callback, topic, time.Now()})
+	return nil
+}
+
+func TestSubscribe(t *testing.T) {
+	assert := assert.New(t)
+	store := &fakeHubStore{}
+
+	h := httptest.NewServer(New(store).Handler())
+	defer h.Close()
+
+	verification := make(chan url.Values, 1)
+
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet && r.URL.Path == "/unguessable-path-unique-per-subscription" {
+			verification <- r.URL.Query()
+			w.Write([]byte("this-is-a-challenge"))
+		}
+	}))
+	defer s.Close()
+
+	resp, err := http.PostForm(h.URL, url.Values{
+		"hub.callback": {s.URL + "/unguessable-path-unique-per-subscription?keep=me"},
+		"hub.mode":     {"subscribe"},
+		"hub.topic":    {"http://example.com/category/cats"},
+	})
+	assert.Nil(err)
+	assert.Equal(http.StatusAccepted, resp.StatusCode)
+
+	select {
+	case v := <-verification:
+		assert.Equal("me", v.Get("keep"))
+		assert.Equal("subscribe", v.Get("hub.mode"))
+		assert.Equal("http://example.com/category/cats", v.Get("hub.topic"))
+		assert.Equal("this-is-a-challenge", v.Get("hub.challenge"))
+		assert.Equal("2419200", v.Get("hub.lease_seconds"))
+	case <-time.After(time.Millisecond):
+		assert.Fail("timed out")
+	}
+
+	if assert.Len(store.subs, 1) {
+		sub := store.subs[0]
+		assert.Equal(s.URL+"/unguessable-path-unique-per-subscription?keep=me", sub.callback)
+		assert.Equal("http://example.com/category/cats", sub.topic)
+		assert.WithinDuration(time.Now().Add(2419200*time.Second), sub.expiresAt, time.Second)
+	}
+}
+
+func TestSubscribeWhenRespondingWithWrongChallenge(t *testing.T) {
+	assert := assert.New(t)
+
+	h := httptest.NewServer(New(nil).Handler())
+	defer h.Close()
+
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet && r.URL.Path == "/unguessable-path-unique-per-subscription" {
+			w.Write([]byte("this-is-not-the-challenge"))
+		}
+	}))
+	defer s.Close()
+
+	resp, err := http.PostForm(h.URL, url.Values{
+		"hub.callback": {s.URL + "/unguessable-path-unique-per-subscription"},
+		"hub.mode":     {"subscribe"},
+		"hub.topic":    {"http://example.com/category/cats"},
+	})
+	assert.Nil(err)
+	assert.Equal(http.StatusBadRequest, resp.StatusCode)
+}
+
+func TestSubscribeNotPostRequest(t *testing.T) {
+	assert := assert.New(t)
+
+	h := httptest.NewServer(New(nil).Handler())
+	defer h.Close()
+
+	resp, err := http.Get(h.URL)
+	assert.Nil(err)
+	assert.Equal(http.StatusMethodNotAllowed, resp.StatusCode)
+}
+
+func TestSubscribeBadCallback(t *testing.T) {
+	assert := assert.New(t)
+
+	h := httptest.NewServer(New(nil).Handler())
+	defer h.Close()
+
+	resp, err := http.PostForm(h.URL, url.Values{
+		"hub.callback": {"this-aint-a-url"},
+		"hub.mode":     {"subscribe"},
+		"hub.topic":    {"http://example.com/category/cats"},
+	})
+	assert.Nil(err)
+	assert.Equal(http.StatusBadRequest, resp.StatusCode)
+}
+
+func TestSubscribeBadMode(t *testing.T) {
+	assert := assert.New(t)
+
+	h := httptest.NewServer(New(nil).Handler())
+	defer h.Close()
+
+	resp, err := http.PostForm(h.URL, url.Values{
+		"hub.callback": {"http://example.com/callback"},
+		"hub.mode":     {"what"},
+		"hub.topic":    {"http://example.com/category/cats"},
+	})
+	assert.Nil(err)
+	assert.Equal(http.StatusBadRequest, resp.StatusCode)
+}
+
+func TestSubscribeBadVerificationResponse(t *testing.T) {
+	assert := assert.New(t)
+
+	h := httptest.NewServer(New(nil).Handler())
+	defer h.Close()
+
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+	}))
+	defer s.Close()
+
+	resp, err := http.PostForm(h.URL, url.Values{
+		"hub.callback": {s.URL},
+		"hub.mode":     {"subscribe"},
+		"hub.topic":    {"http://example.com/category/cats"},
+	})
+	assert.Nil(err)
+	assert.Equal(http.StatusBadRequest, resp.StatusCode)
+}
+
+func TestPublish(t *testing.T) {
+	assert := assert.New(t)
+
+	store := &fakeHubStore{}
+	hub := New(store)
+
+	type request struct {
+		body    string
+		headers http.Header
+	}
+
+	req := make(chan request, 1)
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		data, _ := ioutil.ReadAll(r.Body)
+		req <- request{string(data), r.Header}
+	}))
+	defer s.Close()
+
+	c := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plainest")
+		w.Write([]byte("i-am-content"))
+	}))
+	defer c.Close()
+
+	store.Subscribe(s.URL, c.URL, time.Now().Add(time.Second))
+
+	err := hub.Publish(c.URL)
+	assert.Nil(err)
+
+	select {
+	case r := <-req:
+		assert.Equal("i-am-content", r.body)
+		assert.Equal("text/plainest", r.headers.Get("Content-Type"))
+		assert.Equal(`<the-hub-url>; rel="hub", <`+c.URL+`>; rel="self"`, r.headers.Get("Link"))
+	case <-time.After(time.Millisecond):
+		assert.Fail("timed out")
+	}
+}
+
+func TestPublishReturnsGone(t *testing.T) {
+	assert := assert.New(t)
+
+	store := &fakeHubStore{}
+	hub := New(store)
+
+	type request struct {
+		body    string
+		headers http.Header
+	}
+
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusGone)
+	}))
+	defer s.Close()
+
+	c := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plainest")
+		w.Write([]byte("i-am-content"))
+	}))
+	defer c.Close()
+
+	store.Subscribe(s.URL, c.URL, time.Now().Add(time.Second))
+
+	err := hub.Publish(c.URL)
+	assert.Nil(err)
+
+	if assert.Len(store.unsubs, 1) {
+		unsub := store.unsubs[0]
+
+		assert.Equal(s.URL, unsub.callback)
+		assert.Equal(c.URL, unsub.topic)
+	}
+}
